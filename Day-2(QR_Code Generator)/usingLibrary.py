55bf1be1391da08a1cb348b3068eb67d16c4f182
Hgurura1 <hgurura1@binghamton.edu>
2024-04-06 11:46:51 -0400

    error rectify

--- Day-2(QR_Code Generator)/usingLibrary.py
+++ Day-2(QR_Code Generator)/usingLibrary.py
@@ -9,7 +9,7 @@ app= Flask(__name__)
 def home():
     return render_template('form.html')
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['POST'])
 def generateQR():
     if(request.method == 'POST'):
         memory=BytesIO()
